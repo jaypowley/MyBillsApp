8f2cc321c2726c64a25577da6900977256784289
Jason Powley <jason@jasonpowley.com>
2026-04-21 21:59:17 -0400

    Dockerizing web and sql

--- MyBills.Dacpac/ent/Tables/SqlLog.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [ent].[SqlLog] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [Value]       NVARCHAR (MAX) NULL,
-    [CreatedDate] DATETIME       NOT NULL,
-    CONSTRAINT [PK_ent.SqlLog] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
